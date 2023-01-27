3ba5eb77044a64f818a69c5688c537be45b9fea9
Denis-isnotavailable <92397311+Denis-isnotavailable@users.noreply.github.com>
2023-01-27 23:00:09 +0200

    Update DiscussionArea.jsx

--- src/components/DiscussionArea/DiscussionArea.jsx
+++ src/components/DiscussionArea/DiscussionArea.jsx
@@ -41,9 +41,15 @@ const DiscussionArea = ({kids = []}) => {
             {!isDiscussionOpen ||
                 <ul>{
                     comments.map(com => {
+                        const time = new Date(com.time + 1500000000000);
+                        const date = time.toString().split(' ').slice(1, 5);
                         return (
-                            <li key={com.id} style={{paddingLeft: '36px'}}>
-                                <div>{com.text}</div>
+                            <li key={com.id} style={{ paddingLeft: '36px' }}>
+                                <div>
+                                    <p>Author: {com.by}</p>
+                                    <p>{date.join(' ')}</p>
+                                </div>
+                                <p>{com.text}</p>
                                 <p>---------------------------------------------------</p>
                                 {!com.kids || <DiscussionArea commentId={com.id} kids={com?.kids}/>}
 
